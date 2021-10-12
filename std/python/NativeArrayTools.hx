5a7e295dcdccda4d5ed662b4575c9f2293b39507
k <k@klabz.org>
2021-10-12 23:25:43 +0200

    [python] NativeArrayTools.nativeSort()

--- std/python/NativeArrayTools.hx
+++ std/python/NativeArrayTools.hx
@@ -31,6 +31,10 @@ class NativeArrayTools {
 		python.Syntax.field(a, "append")(x);
 	}
 
+	public static inline function nativeSort<T, TKey>(a:Array<T>, key:T->TKey):Void {
+		python.Syntax.code('{0}.sort(key = {1})', a, key);
+	}
+
 	public static inline function contains<T>(a:Array<T>, x:T):Bool {
 		return python.Syntax.isIn(x, a);
 	}
