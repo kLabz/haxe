2ca596adce39b490db4f877bc64ecb18681bf930
Rudy Ges <k@klabz.org>
2023-03-22 17:56:02 +0100

    [tests] try https test with all targets

--- tests/unit/src/unitstd/Https.unit.hx
+++ tests/unit/src/unitstd/Https.unit.hx
@@ -1,6 +1,2 @@
-#if (cpp || hl || (neko && !macro && !interp))
 var r = haxe.Http.requestUrl("https://raw.githubusercontent.com/HaxeFoundation/haxe/development/tests/unit/res1.txt");
 r == "Héllo World !";
-#else
-"disabled" == "disabled";
-#end
