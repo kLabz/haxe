212f921d5b7dd9240fa124fbcffe992b5967758a
Rudy Ges <k@klabz.org>
2024-02-19 15:24:13 +0100

    [tests] Mac cppia isn't happy there for some reason

--- tests/unit/src/unit/issues/Issue10106.hx
+++ tests/unit/src/unit/issues/Issue10106.hx
@@ -1,13 +1,13 @@
 package unit.issues;
 
-private class CExtension {
-	public static function fromS(cls:Class<C>, s:String) {
-		return new C(s);
+class Issue10106CExtension {
+	public static function fromS(cls:Class<Issue10106C>, s:String) {
+		return new Issue10106C(s);
 	}
 }
 
-@:using(unit.issues.Issue10106.CExtension)
-private class C {
+@:using(unit.issues.Issue10106.Issue10106CExtension)
+class Issue10106C {
 	public final s:String;
 
 	public function new(s:String) {
@@ -15,21 +15,21 @@ private class C {
 	}
 }
 
-private class EnExtension {
-	public static function fromS(en:Enum<En>, st:String):En {
+class Issue10106EnExtension {
+	public static function fromS(en:Enum<Issue10106En>, st:String):Issue10106En {
 		return A;
 	}
 }
 
-@:using(unit.issues.Issue10106.EnExtension)
-private enum En {
+@:using(unit.issues.Issue10106.Issue10106EnExtension)
+enum Issue10106En {
 	A;
 	B;
 }
 
 class Issue10106 extends Test {
 	function test() {
-		eq(A, En.fromS("A"));
-		eq("foo", C.fromS("foo").s);
+		eq(A, Issue10106En.fromS("A"));
+		eq("foo", Issue10106C.fromS("foo").s);
 	}
 }
